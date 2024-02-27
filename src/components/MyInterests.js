107890e0adec0daa83e59f2eeac52c2e7ec06a60
kaini <a1866621@adelaide.edu.au>
2024-02-28 09:52:51 +1030

    feat: CV compressed

--- src/components/MyInterests.js
+++ src/components/MyInterests.js
@@ -46,7 +46,7 @@ const MyInterests = () => {
                 </p>
                 </div>
             </a>
-            <a href='https://kainichang.github.io/KainiChang_CV.pdf' download="KainiChang_CV.pdf" target="_blank" rel="noopener noreferrer">
+            <a href='https://kainichang.github.io/KainiChang_CV_compressed.pdf' download="KainiChang_CV_compressed.pdf" target="_blank" rel="noopener noreferrer">
                 <div className="h-64 w-64 my-4 mx-auto block md:mx-2 overflow-hidden rounded bg-customlime">
                     <img className="h-16 w-16 mt-20 mx-auto"  src={File}/>
                 <p className='font-bold text-customorange bg-transparent py-2 px-4  mb-2 mx-1 mx-auto block md:mx-2'>
